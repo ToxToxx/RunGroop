ff2a7abbd438e485bc9b7d12dad04be69cc1c785
Matvey <sinema3d00@gmail.com>
2024-08-30 11:42:48 +0300

    bring in club repository

---
+++ RunGroupWebApp.Tests/ControllerTest/ClubControllerTests.cs
@@ -0,0 +1,21 @@
+﻿using FakeItEasy;
+using RunGroopWebApp.Interfaces;
+using RunGroopWebApp.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunGroupWebApp.Tests.ControllerTest
+{
+    public class ClubControllerTests
+    {
+        private IClubRepository _clubRepository;
+        public ClubControllerTests() 
+        {
+            _clubRepository = A.Fake<IClubRepository>();
+        }
+        
+    }
+}

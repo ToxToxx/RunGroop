648bfd66dbf8627d8b40b7e9974d079b2a45b151
Matvey <sinema3d00@gmail.com>
2024-08-30 13:18:13 +0300

    bring in all data to start testing and making first test

--- RunGroupWebApp.Tests/ControllerTest/ClubControllerTests.cs
+++ RunGroupWebApp.Tests/ControllerTest/ClubControllerTests.cs
@@ -1,20 +1,46 @@
 ﻿using FakeItEasy;
 using RunGroopWebApp.Interfaces;
+using RunGroopWebApp.Services;
 using RunGroopWebApp.Repository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RunGroopWebApp.Models;
+using Microsoft.AspNetCore.Http;
+using RunGroopWebApp.Controllers;
 
 namespace RunGroupWebApp.Tests.ControllerTest
 {
     public class ClubControllerTests
     {
+        private ClubController _clubController;
         private IClubRepository _clubRepository;
+        private IPhotoService _photoService;
+        private IHttpContextAccessor _httpContexAccessor;
+
         public ClubControllerTests() 
         {
+            //Dependencies
             _clubRepository = A.Fake<IClubRepository>();
+            _photoService = A.Fake<IPhotoService>();
+            _httpContexAccessor = A.Fake<HttpContextAccessor>();
+
+            ///SUT
+            _clubController = new ClubController(_clubRepository, _photoService);
+        }
+
+        [Fact]
+        public void ClubController_Index_ReturnsSuccess()
+        {
+            //Arrange - what needed to bring in
+            var clubs = A.Fake<IEnumerable<Club>>();
+            A.CallTo(() => _clubRepository.GetAll()).Returns(clubs);
+
+            //Act
+
+            //Assert
         }
         
     }

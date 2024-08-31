882906cbab521728018d9d46b5854c4d467ccc54
Matvey <sinema3d00@gmail.com>
2024-08-31 20:15:05 +0300

    Getting some db context to test

--- RunGroupWebApp.Tests/RepositoryTest/ClubRepositoryTests.cs
+++ RunGroupWebApp.Tests/RepositoryTest/ClubRepositoryTests.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using RunGroopWebApp.Data;
+using RunGroopWebApp.Data.Enum;
+using RunGroopWebApp.Models;
+using RunGroopWebApp.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +14,42 @@ namespace RunGroupWebApp.Tests.RepositoryTest
 {
     public class ClubRepositoryTests
     {
+        private async Task<ApplicationDbContext> GetDbContext()
+        {
+            var options =
+                new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+            var databaseContext = new ApplicationDbContext(options);
+            databaseContext.Database.EnsureCreated();
+            if(await databaseContext.Clubs.CountAsync() < 0)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    databaseContext.Clubs.Add(
+                      new Club()
+                      {
+                          Title = "Running Club 1",
+                          Image = "https://www.eatthis.com/wp-content/uploads/sites/4/2020/05/running.jpg?quality=82&strip=1&resize=640%2C360",
+                          Description = "This is the description of the first cinema",
+                          ClubCategory = ClubCategory.City,
+                          Address = new Address()
+                          {
+                              Street = "Ulica Pushkina",
+                              City = "Torez",
+                              State = "DPR"
+                          }
+                      });
+                    await databaseContext.SaveChangesAsync();
+                }
+            }
+            return databaseContext;
+        }
 
+        [Fact]
+        public async void ClubRepository_Add_ReturnsBool()
+        {
+
+        }
     }
 }
